1b85e5c3158648ca1256c5390953d40ecd68376f
Thomas Sojka <thomas.sojka@comsysto.com>
2017-10-12 17:58:53 +0200

    Shorten axios request object

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const shortjson = a => a
+const _ = require('ramda')
+
+const shortjson = _.pipe(
+  _.map(
+    _.cond([
+      [_.is(Object), o => _.pick(_.pipe(Object.keys, _.take(4))(o))(o)],
+      [
+        _.is(String),
+        _.pipe(
+          _.ifElse(
+            _.pipe(_.length, _.gte(60)),
+            _.identity,
+            _.pipe(_.slice(0, 60), _.flip(_.concat)('...'))
+          )
+        )
+      ],
+      [_.T, _.identity]
+    ])
+  ),
+  o => JSON.stringify(o, null, 2)
+)
 
 module.exports = shortjson
--- index.test.js
+++ index.test.js
@@ -4,32 +4,30 @@ const shortjson = require('.')
 
 describe('shortjson ', () => {
   it('shortify axios request', () => {
-    expect(shortjson(fixture)).toEqual(`
-      status: 200,
-      statusText: 'OK',
-      headers: {
-        server: 'openresty',
-        date: 'Tue, 10 Oct 2017 16:17:06 GMT',
-        'content-type': 'text/html; charset=utf-8',
-        'content-length': '148530',
-        ...
-      },
-      config: {
-        transformRequest: {},
-        transformResponse: {},
-        timeout: 0,
-        xsrfCookieName: 'XSRF-TOKEN',
-        ...
-      },
-      request: {
-        domain: null,
-        _events: {},
-        _eventsCount: 5,
-        output: [],
-        ...
-      },
-      data:
-        '<!DOCTYPE html><html xmlns:cc="http://creativecommons.org/ns...'
-    `)
+    expect(shortjson(fixture)).toEqual(
+      `{
+  "status": 200,
+  "statusText": "OK",
+  "headers": {
+    "server": "openresty",
+    "date": "Tue, 10 Oct 2017 16:17:06 GMT",
+    "content-type": "text/html; charset=utf-8",
+    "content-length": "148530"
+  },
+  "config": {
+    "transformRequest": {},
+    "transformResponse": {},
+    "timeout": 0,
+    "xsrfCookieName": "XSRF-TOKEN"
+  },
+  "request": {
+    "domain": null,
+    "_events": {},
+    "_eventsCount": 5,
+    "output": []
+  },
+  "data": "<!DOCTYPE html><html xmlns:cc=\\"http://creativecommons.org/ns..."
+}`
+    )
   })
 })
